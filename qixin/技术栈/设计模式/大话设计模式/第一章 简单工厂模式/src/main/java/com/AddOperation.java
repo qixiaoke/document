bc1399caeeb41b4d94ffa0e39b8b86393a7cd85a
qixin <412411732@qq.com>
2017-06-23 20:46:48 +0800

    factory design pattern

---
+++ qixin/技术栈/设计模式/大话设计模式/第一章 简单工厂模式/src/main/java/com/AddOperation.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * Created by qixin on 2017/6/23.
+ */
+public class AddOperation extends Operation {
+
+    public double getResult() {
+        return getNumber1() + getNumber2();
+    }
+}
