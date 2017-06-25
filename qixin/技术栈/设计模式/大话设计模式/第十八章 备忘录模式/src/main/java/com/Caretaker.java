0efab4e774a321a6a6a411477da00cedf5b54e0f
qixin <412411732@qq.com>
2017-06-25 16:16:45 +0800

    memento design pattern

---
+++ qixin/技术栈/设计模式/大话设计模式/第十八章 备忘录模式/src/main/java/com/Caretaker.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * Created by qixin on 2017/6/25.
+ */
+public class Caretaker {
+    private Memento memento;
+
+    public Memento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(Memento memento) {
+        this.memento = memento;
+    }
+}
---
+++ qixin/技术栈/设计模式/大话设计模式/第十八章 备忘录模式/src/main/java/com/Memento.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * Created by qixin on 2017/6/25.
+ */
+public class Memento {
+    private String state;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
