7652de1bf530ae9667ceca88f4b9aebb552063f9
qixin <412411732@qq.com>
2017-07-22 20:09:50 +0800

    chapter7

---
+++ qixin/技术栈/java/thinking in java/chapter7/src/main/java/FinalTest.java
@@ -0,0 +1,16 @@
+/**
+ * Created by qixin on 2017/7/22.
+ */
+public class FinalTest {
+    private final int i;
+
+    private final int j = 1;
+
+    public FinalTest(int i) {
+        this.i = i;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ qixin/技术栈/java/thinking in java/chapter7/src/main/java/extendInit/ChildClassAutoConstruct.java
@@ -0,0 +1,13 @@
+package extendInit;
+
+/**
+ * Created by qixin on 2017/7/22.
+ */
+public class ChildClassAutoConstruct extends FatherClassAutoConstruct {
+    public ChildClassAutoConstruct() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
